9c6a055573ad2715b2bbc7b6b0b5ac7ad7f6ceaf
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-08-14 13:37:19 +0300

    Update in the updateListGoodsMetrics function: - created the skuFilteredBySkuNameAndId variable, which is the result of filtering by sku name and id and moved the filtering to the top of the loop - if the skuFilteredBySkuNameAndId variable does not have a property in the metrics array, then assign it

--- src/server/routes/reports/services/different/updateListGoodsMetrics.js
+++ src/server/routes/reports/services/different/updateListGoodsMetrics.js
@@ -30,45 +30,48 @@ var updateListGoodsMetrics = (report, listGoods) => {
     return { listGoodsWithUpdatedSkuMetrics: [] };
   }
 
+  var { year } = report.recordedTo;
+
   if (report.isCrossYearPeriod) {
-    var startYear = +report.dateFrom.split("-")[0];
+    var startYear = year;
     var endYear = +report.dateTo.split("-")[0];
   }
 
-  var { year } = report.recordedTo;
-
   for (var sku of report.skus) {
+    var skuFilteredBySkuNameAndId = listGoods.find((i) => i.id === sku.id && i.skuName === sku.skuName);
+
+    if (!skuFilteredBySkuNameAndId?.metrics) {
+      skuFilteredBySkuNameAndId.metrics = [];
+    }
+
+    var skuMetrics = skuFilteredBySkuNameAndId.metrics;
+
     if (report.isCrossYearPeriod) {
-      var skuMetrics = listGoods.find((i) => i.id === sku.id && i.skuName === sku.skuName)?.metrics;
+      var startYearMetric = skuMetrics.find((i) => i.year === startYear);
 
-      if (skuMetrics?.length) {
-        var startYearMetric = skuMetrics.find((i) => i.year === startYear);
-
-        if (!startYearMetric) {
-          startYearMetric = { year: startYear, ...defaultSkuMetricsField };
-        }
-
-        var endYearMetric = skuMetrics.find((i) => i.year === endYear);
-
-        if (!endYearMetric) {
-          endYearMetric = { year: endYear, ...defaultSkuMetricsField };
-        }
-
-        startYearMetric = aggregateSkuMetrics(startYearMetric, sku, startYearPropPostfix);
-        endYearMetric = aggregateSkuMetrics(endYearMetric, sku, endYearPropPostfix);
+      if (!startYearMetric) {
+        startYearMetric = { year: startYear, ...defaultSkuMetricsField };
       }
+
+      startYearMetric = aggregateSkuMetrics(startYearMetric, sku, startYearPropPostfix);
+
+      var endYearMetric = skuMetrics.find((i) => i.year === endYear);
+
+      if (!endYearMetric) {
+        endYearMetric = { year: endYear, ...defaultSkuMetricsField };
+      }
+
+      endYearMetric = aggregateSkuMetrics(endYearMetric, sku, endYearPropPostfix);
     } else {
-      var skuMetrics = listGoods.find((i) => i.id === sku.id && i.skuName === sku.skuName)?.metrics;
+      var skuMetrics = skuFilteredBySkuNameAndId.metrics;
 
-      if (skuMetrics?.length) {
-        var skuMetric = skuMetrics.find((i) => i.year === year);
+      var skuMetric = skuMetrics.find((i) => i.year === year);
 
-        if (!skuMetric) {
-          skuMetric = { year, ...defaultSkuMetricsField };
-        }
-
-        skuMetric = aggregateSkuMetrics(skuMetric, sku);
+      if (!skuMetric) {
+        skuMetric = { year, ...defaultSkuMetricsField };
       }
+
+      skuMetric = aggregateSkuMetrics(skuMetric, sku);
     }
   }
 
