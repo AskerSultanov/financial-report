18dfed1aee0b6f6eacc317790cf3973a8c0ae354
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-08-19 09:23:19 +0300

    added unique attribute to Users model

--- src/server/database/schemas/users.js
+++ src/server/database/schemas/users.js
@@ -2,10 +2,10 @@ import { Schema } from "mongoose";
 
 var usersSchema = new Schema({
   schemaVersion: { type: Number },
-  login: { type: String, required: true },
-  userId: { type: String, required: true },
   passwd: { type: String, required: true },
   registeredAt: { type: Date, required: true },
+  login: { type: String, required: true, unique: true },
+  userId: { type: String, required: true, unique: true },
   role: { type: String, required: true, default: "user", enum: ["user", "admin"] },
 });
 
