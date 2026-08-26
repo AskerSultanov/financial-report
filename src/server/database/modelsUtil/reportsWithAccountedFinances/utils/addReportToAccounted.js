aaa3f9a0bbeba4c2cb9c1ff99b97f7d46bc4ebd9
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-08-26 18:28:22 +0300

    added a change to the financial accounting status for the “reports” collection.

--- src/server/database/modelsUtil/reportsWithAccountedFinances/utils/addReportToAccounted.js
+++ src/server/database/modelsUtil/reportsWithAccountedFinances/utils/addReportToAccounted.js
@@ -1,14 +1,24 @@
-import { reportsWithAccountedFinancesModel } from "../../../models/index.js";
+import { dbClient } from "../../../index.js";
+import { reportModel, reportsWithAccountedFinancesModel } from "../../../models/index.js";
 
 var mskTimeOffsetInMs = 10_800_000;
 
 var addReportToAccounted = async (userId, reportId, dateFrom, dateTo) => {
-  return await reportsWithAccountedFinancesModel.create({
-    userId,
-    reportId,
-    dateFrom,
-    dateTo,
-    financesAccountedAt: Date.now() + mskTimeOffsetInMs,
+  var session = await dbClient.startSession();
+
+  return await session.withTransaction(async () => {
+    await reportModel.updateOne({ userId, reportId }, { isFinancesAccounted: true }, { session: session });
+
+    await reportsWithAccountedFinancesModel.create(
+      {
+        userId,
+        reportId,
+        dateFrom,
+        dateTo,
+        financesAccountedAt: Date.now() + mskTimeOffsetInMs,
+      },
+      { session: session },
+    );
   });
 };
 
--- src/server/database/modelsUtil/reportsWithAccountedFinances/utils/removeReportFromAccounted.js
+++ src/server/database/modelsUtil/reportsWithAccountedFinances/utils/removeReportFromAccounted.js
@@ -1,7 +1,13 @@
-import { reportsWithAccountedFinancesModel } from "../../../models/index.js";
+import { dbClient } from "../../../index.js";
+import { reportModel, reportsWithAccountedFinancesModel } from "../../../models/index.js";
 
 var removeReportFromAccounted = async (userId, reportId) => {
-  return await reportsWithAccountedFinancesModel.deleteOne({ userId, reportId });
+  var session = await dbClient.startSession();
+
+  return await session.withTransaction(async () => {
+    await reportsWithAccountedFinancesModel.deleteOne({ userId, reportId }, { session: session });
+    await reportModel.updateOne({ userId, reportId }, { isFinancesAccounted: false }, { session: session });
+  });
 };
 
 export default removeReportFromAccounted;
