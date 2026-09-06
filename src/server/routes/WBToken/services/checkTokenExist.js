d0c966d19fea3a0be0f37fb5d435f834672c027d
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-06 20:42:11 +0300

    implemented method prismaServices.tokenModelServices.getWBTokenByUserId into checkTokenExistService

--- src/server/routes/WBToken/services/checkTokenExist.js
+++ src/server/routes/WBToken/services/checkTokenExist.js
@@ -2,10 +2,14 @@ import parseJwt from "./utils/parseJwt.js";
 import checkTokenExpiry from "./utils/checkTokenExpiry.js";
 import { getWBTokenByUserId } from "../../../database/modelsUtil/tokens/index.js";
 
+import * as prismaServices from "../../../postgres/services/index.js";
+
 var checkTokenExistService = async (userId) => {
   var { token } = await getWBTokenByUserId(userId);
+  var tokenFromPg =
+    await prismaServices.tokenModelServices.getWBTokenByUserId(userId);
 
-  if (!token) {
+  if (!token && !tokenFromPg.token) {
     return { tokenIsMissing: true, isExpired: false, token: "" };
   }
 
