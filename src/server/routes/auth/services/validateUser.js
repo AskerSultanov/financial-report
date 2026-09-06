2f40103b1137310515e2078d827ff54e1818df20
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-06 19:10:11 +0300

    implemented pg method prismaServices.userModelServices.getUserByLogin(login) into valitedUser func

--- src/server/routes/auth/services/validateUser.js
+++ src/server/routes/auth/services/validateUser.js
@@ -1,5 +1,6 @@
 import argon2 from "argon2";
 import dbUtils from "../../../database/modelsUtil/index.js";
+import * as prismaServices from "../../../postgres/services/index.js";
 
 var { getUserByLogin } = dbUtils.userModelUtils;
 
@@ -7,12 +8,13 @@ var validateUser = async (login, pwd) => {
   var credentialInvalid = true;
 
   var user = await getUserByLogin(login);
+  var userFromPg = await prismaServices.userModelServices.getUserByLogin(login);
 
-  if (!user) {
+  if (!user && !userFromPg) {
     return { credentialInvalid, userId: null };
   }
 
-  credentialInvalid = !await argon2.verify(user.passwd, pwd);
+  credentialInvalid = !(await argon2.verify(user.passwd, pwd));
 
   return { credentialInvalid, userId: user.userId };
 };
