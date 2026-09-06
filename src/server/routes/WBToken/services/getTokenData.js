11f701e975c6b9c4eca59be5840a97d7a62f461d
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-06 20:41:33 +0300

    implemented method prismaServices.tokenModelServices.getWBTokenByUserId into getTokenDataService

--- src/server/routes/WBToken/services/getTokenData.js
+++ src/server/routes/WBToken/services/getTokenData.js
@@ -2,10 +2,14 @@ import parseJwt from "./utils/parseJwt.js";
 import getTokenDetails from "./utils/getTokenDetails.js";
 import { getWBTokenByUserId } from "../../../database/modelsUtil/tokens/index.js";
 
+import * as prismaServices from "../../../postgres/services/index.js";
+
 var getTokenDataService = async (userId) => {
   var { token, lastUsed } = await getWBTokenByUserId(userId);
+  var tokenFromPg =
+    await prismaServices.tokenModelServices.getWBTokenByUserId(userId);
 
-  if (!token.length) {
+  if (!token.length && !tokenFromPg.token.length) {
     return { tokenIsExist: false, tokenDetails: null };
   }
 
