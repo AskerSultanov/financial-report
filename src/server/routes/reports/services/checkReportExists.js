6e53aa7d9fcdd97780caff48082473c02fdd2822
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-06 20:20:55 +0300

    implemented pg method getReportFromEmptyPeriods and checkReportExistByDate into checkReportExistsService

--- src/server/routes/reports/services/checkReportExists.js
+++ src/server/routes/reports/services/checkReportExists.js
@@ -1,24 +1,39 @@
 import dbUtils from "../../../database/modelsUtil/index.js";
+import * as prismaServices from "../../../postgres/services/index.js";
 
 var { checkReportExistByDate } = dbUtils.reportPeriodsModelUtils;
 var { getEmptyReportPeriods } = dbUtils.reportLoadingStateModelUtils;
 
 var checkReportExistsService = async (data) => {
-  var { userId, dateFrom } = data;
+  var { userId, dateFrom, dateTo } = data;
 
   var { emptyReportPeriods } = await getEmptyReportPeriods(userId);
+
+  var reportFromEmptyPeriods =
+    await prismaServices.emptyReportPeriodsModelServices.getReportFromEmptyPeriods(
+      userId,
+      dateFrom,
+      dateTo,
+    );
 
   var reportPeriodExistInEmptyPeriods = emptyReportPeriods.find(
     (item) => item.dateFrom === dateFrom,
   );
 
-  if (reportPeriodExistInEmptyPeriods) {
+  if (reportPeriodExistInEmptyPeriods && reportFromEmptyPeriods) {
     return { reportIsEmpty: true, reportIsExist: false };
   }
 
   var report = await checkReportExistByDate(userId, dateFrom);
 
-  if (report) {
+  var reportFromPg =
+    await prismaServices.reportPeriodsModelServices.checkReportExistByDate(
+      userId,
+      dateFrom,
+      dateTo,
+    );
+
+  if (report && reportFromPg) {
     return { reportIsEmpty: false, reportIsExist: true };
   }
 
