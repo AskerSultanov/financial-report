6361a303927215e7faeb762ebe499be99e9cc473
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-07 10:52:58 +0300

    Moved the user deletion controller from the router /delete to the router /personal-account

--- src/server/routes/delete/index.js
+++ src/server/routes/delete/index.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import deleteUserController from "./controllers/deleteUser.js";
 import deleteUsersController from "./controllers/deleteUsers.js";
 
 var router = Router({ caseSensitive: true, strict: true });
 
-router.delete("/user", deleteUserController);
+
 router.delete("/users", deleteUsersController);
 
 export default router;
--- src/server/routes/personalAccount/index.js
+++ src/server/routes/personalAccount/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-
+import deleteUserController from "./controllers/deleteUser.js";
 import resetUserDataController from "./controllers/resetUserData.js";
 
 var router = Router({ caseSensitive: true, strict: true });
 
 router.post("/", resetUserDataController);
+router.delete("/", deleteUserController);
 
 export default router;
