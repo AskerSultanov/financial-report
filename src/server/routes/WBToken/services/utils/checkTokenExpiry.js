8b3ab0c30f53f0e76caf216535e41a84c4d46eba
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-07 10:16:41 +0300

    Updated the checkTokenExpiry function: - now it does not check the validity of tokenPayload

--- src/server/routes/WBToken/services/utils/checkTokenExpiry.js
+++ src/server/routes/WBToken/services/utils/checkTokenExpiry.js
@@ -1,17 +1,9 @@
 var msInSec = 1000;
 
 var checkTokenExpiry = (tokenPayload) => {
-  var tokenExp = tokenPayload?.exp;
-
-  if (!tokenExp || typeof tokenExp !== "number" || isNaN(tokenExp)) {
-    throw new Error(
-      "Invalid WBTOKEN payload: exp property is missing or invalid",
-    );
-  }
-
   var currentTimestamp = Date.now();
 
-  var isExpired = tokenExp * msInSec <= currentTimestamp;
+  var isExpired = tokenPayload.exp * msInSec <= currentTimestamp;
 
   return { isExpired };
 };
